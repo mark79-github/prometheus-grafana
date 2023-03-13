f3b89b2a0985a1d7824236edb8361132e3d79cd1
Martin Dimitrov <52877024+mark79-github@users.noreply.github.com>
2023-03-13 16:39:38 +0200

    Update setup-stack.sh

--- setup-stack.sh
+++ setup-stack.sh
@@ -3,3 +3,9 @@
 echo "*** Deploy stack (prometheus, grafana, goprom - etc.) ..."
 cd /vagrant && docker compose down || true
 docker compose up -d
+
+echo "*** Generate requests to worker1 ..."
+/vagrant/goprom/runner.sh http://192.168.34.101:8081 &> /tmp/runner8081.log &
+
+echo "*** Generate requests to worker2 ..."
+/vagrant/goprom/runner.sh http://192.168.34.101:8082 &> /tmp/runner8082.log &
